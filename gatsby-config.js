dd9ffaf63dd28c93b017012b2c22c24bbf7f6844
Ujjwal Sharma <usharma1998@gmail.com>
2018-03-29 01:46:59 +0530

    Change prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
   },
-  pathPrefix: '/gatsby-starter-blog',
+  pathPrefix: '/blog',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
